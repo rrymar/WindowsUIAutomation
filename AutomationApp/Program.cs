8ceea962ed836f0752e2876f67d1dc08214606ff
RuslanRymar <rrimar@idiq.com>
2022-11-18 14:32:09 +0200

    Automation code

---
+++ AutomationApp/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace AutomationApp
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ AutomationApp/Tests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+
+namespace AutomationApp
+{
+    [TestClass]
+    public class Tests : AppSession
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+
+            var elements = session.FindElementsByXPath(@"//*");
+
+            var textBoxes = session.FindElementsByTagName("Edit");
+            
+            textBoxes[0].SendKeys("some text");
+            textBoxes[1].SendKeys("some text 2");
+            
+            var checkBox1 = session.FindElementByName("checkBox1");
+            checkBox1.Click();
+            
+            var button = session.FindElementByName("button1"); 
+            button.Click();
+            button.Click();
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+        }
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            Setup(context);
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            TearDown();
+        }
+    }
+}
