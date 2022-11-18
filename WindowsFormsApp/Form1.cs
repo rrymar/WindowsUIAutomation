d1d9818ce3b19b8c794f6b06e41d80c0fe71fc85
RuslanRymar <rrimar@idiq.com>
2022-11-18 13:08:06 +0200

    add test winforms app

---
+++ WindowsFormsApp/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        private int clicks = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            clicks++;
+            label1.Text = @"Button Clicked " + clicks;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
